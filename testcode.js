81bc196197ca132f615df8c661d7a198d017c0c7
mhmd <ms.mhmdsalah@gmail.com>
2024-06-03 19:49:57 +0300

    learning

--- testcode.js
+++ testcode.js
@@ -1,3 +1,26 @@
-console.time("alert")
-alert("hello")
-console.timeEnd("alert")
+// let containere = document.querySelector(".container")
+
+// let animates = document.querySelector(".anim")
+// let span = document.querySelector("span")
+// setTimeout(ef,2500)
+
+// function ef ()
+// {
+//     container.innerHTML = "hello  mohamed slah"
+//     animates.classList.remove("anim")
+// }
+
+// document.body.addEventListener( "contextmenu", ( e ) =>
+// {
+//     // console.log( window.innerWidth )
+//     // console.log(e.offsetHeight)
+//     console.log(e.pageX)
+// })
+
+let container = document.querySelector( ".container" );
+let button = document.querySelector( ".container button" );
+
+button.addEventListener( "click", (e) =>
+{
+    e.target.parentElement.parentElement.remove()
+} );
