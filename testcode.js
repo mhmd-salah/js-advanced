620fc9cc383a2f1cee6e9b92941b399805734de0
mhmd <ms.mhmdsalah@gmail.com>
2024-06-06 13:49:58 +0300

    -

--- testcode.js
+++ testcode.js
@@ -17,10 +17,27 @@
 //     console.log(e.pageX)
 // })
 
-let container = document.querySelector( ".container" );
-let button = document.querySelector( ".container button" );
+// let container = document.querySelector(".container");
+// let button = document.querySelector( ".container button" );
 
-button.addEventListener( "click", (e) =>
-{
-    e.target.parentElement.parentElement.remove()
-} );
+// button.addEventListener( "click", (e) =>
+// {
+//     e.target.parentElement.parentElement.remove()
+// } );
+
+// let rect = container.getBoundingClientRect();
+// console.log(rect)
+
+let box = document.querySelector(".box")
+let rect = box.getBoundingClientRect()
+console.log(rect.x)
+console.log(rect.y)
+console.log(rect.width)
+console.log(rect.left)
+console.log(rect.bottom)
+console.log(box.outerHTML)
+console.log(box.checkVisibility())
+console.log(window.innerHeight )
+console.log(window.innerHeight / 5 * 4)
+box.style.top = `${(window.innerHeight / 5) * 4}`;
+console.log(box.getClientRects())
